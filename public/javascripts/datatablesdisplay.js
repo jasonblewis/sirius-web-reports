ca19bd1f193a2936ea9317703d8fbf7cf367622d
Jason Lewis <jason@dickson.st>
2016-02-09 17:21:39 +1100

    fix datatables to allow save CSV

--- public/javascripts/datatablesdisplay.js
+++ public/javascripts/datatablesdisplay.js
@@ -1,62 +1,8 @@
-var editor; // use a global for the submit and return data rendering in the examples
- 
 $(document).ready(function() {
-    editor = new $.fn.dataTable.Editor( {
-        table: "table.display",
-        fields: [ {
-                label: "First name:",
-                name: "first_name"
-            }, {
-                label: "Last name:",
-                name: "last_name"
-            }, {
-                label: "Position:",
-                name: "position"
-            }, {
-                label: "Office:",
-                name: "office"
-            }, {
-                label: "Extension:",
-                name: "extn"
-            }, {
-                label: "Start date:",
-                name: "start_date",
-                type: "datetime"
-            }, {
-                label: "Salary:",
-                name: "salary"
-            }
-        ]
-    } );
- 
     $('table.display').DataTable( {
-        columns: [
-            { data: null, render: function ( data, type, row ) {
-                // Combine the first and last names into a single table field
-                return data.first_name+' '+data.last_name;
-            } },
-            { data: "position" },
-            { data: "office" },
-            { data: "extn" },
-            { data: "start_date" },
-            { data: "salary", render: $.fn.dataTable.render.number( ',', '.', 0, '$' ) }
-        ],
-        select: true,
+        dom: 'Bfrtip',
         buttons: [
-            { extend: 'create', editor: editor },
-            { extend: 'edit',   editor: editor },
-            { extend: 'remove', editor: editor },
-            {
-                extend: 'collection',
-                text: 'Export',
-                buttons: [
-                    'copy',
-                    'excel',
-                    'csv',
-                    'pdf',
-                    'print'
-                ]
-            }
+            'copy', 'csv', 'excel', 'pdf', 'print'
         ]
     } );
 } );
