177456ccf31d1f3121c680ef4b9ad4a3c426ec50
Jason Lewis <jason@dickson.st>
2016-02-19 15:04:51 +1100

    more work on get-selection

--- lib/Reports/Purchasing.pm
+++ lib/Reports/Purchasing.pm
@@ -11,23 +11,54 @@ sub menu {
   template 'purchasing/purchasing';
 };
 
+sub sales_history_get_primary_supplier {
+
+      my $sql = q{Set transaction isolation level read uncommitted;
+  select  distinct rtrim(s.supplier_code) as 'supplier_code',
+          rtrim(c.name) as 'name'
+  		 from ap_supplier s 
+  join in_product p on
+    p.primary_supplier = s.supplier_code
+  join in_warehouse_product wp on
+    wp.supplier_code = p.primary_supplier
+  join company c
+    on s.company_code = c.company_code 
+  where 
+  ( s.spare_flag_01 is null or s.spare_flag_01 != 'N') 
+  and
+  ( p.spare_flag_03 is null or p.spare_flag_03 = 'Y')
+  and not (wp.reorder_type = 'Q' and wp.reorder_class = 'Q')
+  order by supplier_code
+};
+  my $sth = database->prepare($sql) or die "can't prepare\n";
+  $sth->execute or die $sth->errstr;
+  my $fields = $sth->{NAME};
+  my $rows = $sth->fetchall_arrayref({});
+  $sth->finish;
+
+
+  template 'utils/get-selection', {
+    'target' => request->uri,
+    'rows' => $rows,
+  };
+};
+
 sub sales_history {
+
   my $sth;
   my $sth_dbname;
   my $dat;
   my $dbnames;
-  unless  (query_parameters->get('primary_supplier')) {
-    get_selection('/purchasing/sales-history'); # get the user selection of which supplier they want
-  } else {
-    $sth = database->prepare("select \@\@servername as name")  or die "Can't prepare: $DBI::errstr\n";
-    $sth->execute or die $sth->errstr;
-    $dat = $sth->fetchall_arrayref({});
-    $sth->finish;
-    
-    $sth_dbname = database->prepare("select DB_NAME() as name;") or die "can't prepare\n";
-    $sth_dbname->execute or die $sth_dbname->errstr;
-    $dbnames = $sth_dbname->fetchall_arrayref({});
-    $sth_dbname->finish;
+
+  $sth = database->prepare("select \@\@servername as name")  or die "Can't prepare: $DBI::errstr\n";
+  $sth->execute or die $sth->errstr;
+  $dat = $sth->fetchall_arrayref({});
+  $sth->finish;
+  
+  $sth_dbname = database->prepare("select DB_NAME() as name;") or die "can't prepare\n";
+  $sth_dbname->execute or die $sth_dbname->errstr;
+  $dbnames = $sth_dbname->fetchall_arrayref({});
+  $sth_dbname->finish;
     
     my $sql = q{Set transaction isolation level read uncommitted;
 SELECT
@@ -88,26 +119,26 @@ join in_reorder_class rc
     rc.reorder_type = wp.reorder_type
 where ltrim(rtrim(p.primary_supplier)) = ? ;
 };
-    $sth = database->prepare($sql) or die "can't prepare\n";
-    $sth->bind_param(1,query_parameters->get('primary_supplier'));
-    $sth->execute or die $sth->errstr;
-    my $fields = $sth->{NAME};
-    my $rows = $sth->fetchall_arrayref({});
-    $sth->finish;
-    
-    template 'purchasing/sales-history.tt', {
-      'title' => 'Sales History',
-      'servers' => $dat,
-      'databases' => $dbnames,
-      'fields' => $fields,
-      'rows' => $rows,
-    };
-  }; #if
+  $sth = database->prepare($sql) or die "can't prepare\n";
+  $sth->bind_param(1,query_parameters->get('primary_supplier'));
+  $sth->execute or die $sth->errstr;
+  my $fields = $sth->{NAME};
+  my $rows = $sth->fetchall_arrayref({});
+  $sth->finish;
+  
+  template 'purchasing/sales-history.tt', {
+    'title' => 'Sales History',
+    'servers' => $dat,
+    'databases' => $dbnames,
+    'fields' => $fields,
+    'rows' => $rows,
+  };
 };
 
 prefix '/purchasing' => sub {
   get ''                             => require_login \&menu;
-  get '/sales-history'  => require_login \&sales_history;
+  get '/sales-history'  => require_login \&sales_history_get_primary_supplier;
+  get '/sales-history?:primary_supplier'  => require_login \&sales_history;
 };
 
 1;
