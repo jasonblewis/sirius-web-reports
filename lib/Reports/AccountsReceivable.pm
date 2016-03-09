a5d63b92af9507849e89a675e3c850280a4f65c5
Jason Lewis <jason@dickson.st>
2016-03-09 16:18:35 +1100

    outstanding invoices report

--- lib/Reports/AccountsReceivable.pm
+++ lib/Reports/AccountsReceivable.pm
@@ -6,8 +6,8 @@ use Dancer2::Plugin::Ajax;
 use Dancer2::Plugin::Auth::Extensible;
 use Dancer2::Plugin::DBIC qw(schema resultset rset);
 
-use Data::Dumper;
-local $Data::Dumper::Freezer = '_dumper_hook';
+#use Data::Dumper;
+#local $Data::Dumper::Freezer = '_dumper_hook';
  
 sub menu {
   template 'ar/accounts-receivable';
@@ -21,28 +21,29 @@ sub outstanding_invoices_json {
 
   my @invoices  = schema->resultset('ArTransaction')->invoices->outstanding()->search(undef,
    {
-     prefetch => { ar_customer => 'company' },
+     prefetch => [{ ar_customer => 'company' },
+		  { ar_debtor => 'company'}],
      '+select' => [
        { '' => \'ltrim(str(trans_amt,25,2))', '-as' => 'trans_amt_rounded'},
        { '' => \'CONVERT(VARCHAR(10),trans_date,103)', '-as' => 'trans_date_datepart'},
        { '' => \'CONVERT(VARCHAR(10),due_date,103)', '-as' => 'due_date_datepart'}
      ],
 
- })->rows(30)->hri;
+ })->hri;
 
-  print Dumper(@invoices);
+#  print Dumper(@invoices);
   
   to_json {
+    pageLength => 50,
     columns => [
-         { data => "batch_nr",     title => 'Batch Number',  className => "dt-right"},
-         { data => "debtor_code",  title => 'Debtor Code',   className => "dt-left"},
-         { data => "customer_code",title => 'Customer code', className => "dt-left"},
-	 { data => "trans_date_datepart",  title => 'Transaction<br />Date',      className => "dt-left"},
-	 { data => "trans_amt_rounded", title => 'Transaction<br />Amount', className => "dt-right"},
-	 { data => "ref_1",             title => 'Ref1', className => "dt-left"},
-	 { data => "ref_2",             title => 'Ref2', className => "dt-left"},
-	 { data => "due_date_datepart",          title => 'Due Date', className => "dt-left"},
-	 { data => "ar_customer.company.name",          title => 'customer name', className => "dt-left"},
+         { data => "batch_nr",                 title => 'Batch<br />Number',       className => "dt-right"},          
+	 { data => "ar_debtor.company.name",   title => 'Debtor',                  className => "dt-left"},
+	 { data => "ar_customer.company.name", title => 'Customer',                className => "dt-left"},
+	 { data => "trans_date_datepart",      title => 'Transaction<br />Date',   className=> "dt-right"},
+	 { data => "trans_amt_rounded",        title => 'Transaction<br />Amount', className => "dt-right"},
+	 { data => "ref_1",                    title => 'Ref1',                    className => "dt-left"},
+	 { data => "ref_2",                    title => 'Ref2',                    className => "dt-left"},
+	 { data => "due_date_datepart",        title => 'Due Date',                className => "dt-left"},
        ],
     data => [@invoices]};
 
