928f3d65b756a48eab6cbbdb2c32ad0154775143
Jason Lewis <jason@dickson.st>
2019-05-22 15:42:32 +1000

    use primary_supplier not supplier from sh

--- sql/zz_monthly_sales_2.sql
+++ sql/zz_monthly_sales_2.sql
@@ -12,22 +12,29 @@ create view [dbo].[zz_sh_monthly_sales_2] as
 
 with basequery as (
 
-select supplier_code,
- product_code,
- sum(sales_qty) as sales_qty,
- datediff(month, invoice_date, DATEADD(MONTH, DATEDIFF(MONTH, 0, GETDATE()), 0)) as [age]
+select p.primary_supplier,
+ tv.product_code,
+ sum(tv.sales_qty) as sales_qty,
+ datediff(month, tv.invoice_date, DATEADD(MONTH, DATEDIFF(MONTH, 0, GETDATE()), 0)) as [age]
  
-  from sh_select_trans_view
-  where supplier_code is not null and
-  supplier_code != '"' 
-  and supplier_code != 'ZZ-PROFILE'
-  and supplier_code != 'ZUNK'
-  and invoice_date >= DATEADD(MONTH, DATEDIFF(MONTH, 0, GETDATE())-6, 0) -- rolling 13 month
-  group by supplier_code,product_code,datediff(month, invoice_date, DATEADD(MONTH, DATEDIFF(MONTH, 0, GETDATE()), 0))
+  from
+    sh_select_trans_view tv
+  join
+    in_product p 
+  on
+    tv.product_code = p.product_code
+
+  where tv.supplier_code is not null and
+  tv.supplier_code != '"' 
+  and tv.supplier_code != 'ZZ-PROFILE'
+  and tv.supplier_code != 'ZUNK'
+  and tv.invoice_date >= DATEADD(MONTH, DATEDIFF(MONTH, 0, GETDATE())-6, 0) -- rolling 13 month
+  --and  tv.product_code =  '4111001'
+  group by p.primary_supplier, tv.product_code,datediff(month, tv.invoice_date, DATEADD(MONTH, DATEDIFF(MONTH, 0, GETDATE()), 0))
   
  ) 
  select 
- supplier_code,[product_code],[0],[1],[2],[3],[4],[5]
+ primary_supplier as supplier_code,[product_code],[0],[1],[2],[3],[4],[5]
  from basequery
  pivot
   (
@@ -40,7 +47,8 @@ select supplier_code,
   [2] is  null and
   [3] is  null and
   [4] is  null and
-  [5] is  null )
+  [5] is  null 
+  )
   
  
 GO
